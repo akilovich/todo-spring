48bbfe2d834937258890c7a2742f72ef7af8637f
Nikita Akilov <nikita.n.akilov@gmail.com>
2020-08-09 21:50:20 +0300

    style quickfix

--- src/main/java/com/github/akilovich/Application.java
+++ src/main/java/com/github/akilovich/Application.java
@@ -3,9 +3,20 @@ package com.github.akilovich;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class of the application.
+ */
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    private Application() {
+    }
+
+    /**
+     * Starts the application.
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
